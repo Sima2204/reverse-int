fdddb8ed5232077cb7ff8224aeca87dd55c46e69
Sima2204 <vlad.simuta@gmail.com>
2022-06-23 08:19:03 +0300

    fix: in one line made function

--- src/index.js
+++ src/index.js
@@ -1,5 +1,3 @@
 module.exports = function reverse (n) {
-  let revNum = n.toSrting().split("").reverse().join("");
-  let result = parseInt(revNum);
-  return result;
+  return parseInt(n.toSrting().split('').reverse().join(''));
 };
